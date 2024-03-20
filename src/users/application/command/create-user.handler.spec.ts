538a9193872fc0533d1ba365928d37437858ba99
Lee seongwon <jsw6357@naver.com>
2024-03-20 15:38:32 +0900

    add:login.handler.spec.ts 추가

--- src/users/application/command/create-user.handler.spec.ts
+++ src/users/application/command/create-user.handler.spec.ts
@@ -1,92 +1,92 @@
-import * as uuid from 'uuid';
-import { CreateUserHandler } from './create-user.handler';
-import { Test } from '@nestjs/testing';
-import { CreateUserCommand } from './create-user.command';
-import { EventBus } from '@nestjs/cqrs';
-import { UserEntity } from 'src/users/infra/db/entities/user.entity';
-import { getConnectionToken, getRepositoryToken } from '@nestjs/typeorm';
-import { UserRepository } from 'src/users/infra/db/repository/UserRepository';
-import { UserFactory } from 'src/users/domain/user.factory';
-/*
-NOTE:
-서적과 동일하게 진행하였지만, 테스트 모듈이 DataSource, UserEntity 등을 찾을 수 없다는 에러가 발생
-(CreateUserHandler 클래스에 주입받은 DataSource와 usersRepository: Repository<UserEntity>를 테스트 환경에서 주입하지 않아서 에러 발생)
+// import * as uuid from 'uuid';
+// import { CreateUserHandler } from './create-user.handler';
+// import { Test } from '@nestjs/testing';
+// import { CreateUserCommand } from './create-user.command';
+// import { EventBus } from '@nestjs/cqrs';
+// import { UserEntity } from 'src/users/infra/db/entities/user.entity';
+// import { getConnectionToken, getRepositoryToken } from '@nestjs/typeorm';
+// import { UserRepository } from 'src/users/infra/db/repository/UserRepository';
+// import { UserFactory } from 'src/users/domain/user.factory';
+// /*
+// NOTE:
+// 서적과 동일하게 진행하였지만, 테스트 모듈이 DataSource, UserEntity 등을 찾을 수 없다는 에러가 발생
+// (CreateUserHandler 클래스에 주입받은 DataSource와 usersRepository: Repository<UserEntity>를 테스트 환경에서 주입하지 않아서 에러 발생)
 
-해당 클레스에 의존성 주입받은 객체들을 모두 테스트 환경에서 모크하여 해결
+// 해당 클레스에 의존성 주입받은 객체들을 모두 테스트 환경에서 모크하여 해결
 
-TODO:
-userRepository.save를 찾지를 못함...
- ● CreateUserHandler › execute › should execute CreateUserCommand
+// TODO:
+// userRepository.save를 찾지를 못함...
+//  ● CreateUserHandler › execute › should execute CreateUserCommand
 
-    expect(jest.fn()).toHaveBeenCalled()
+//     expect(jest.fn()).toHaveBeenCalled()
 
-    Expected number of calls: >= 1
-    Received number of calls:    0
-*/
+//     Expected number of calls: >= 1
+//     Received number of calls:    0
+// */
 
-jest.mock('uuid');
-jest.spyOn(uuid, 'v1').mockReturnValue('0000-0000-0000-0000');
+// jest.mock('uuid');
+// jest.spyOn(uuid, 'v1').mockReturnValue('0000-0000-0000-0000');
 
-describe('CreateUserHandler', () => {
-  let createUserHandler: CreateUserHandler;
-  let userRepository: UserRepository;
-  let userFactory: UserFactory;
+// describe('CreateUserHandler', () => {
+//   let createUserHandler: CreateUserHandler;
+//   let userRepository: UserRepository;
+//   let userFactory: UserFactory;
 
-  beforeAll(async () => {
-    const module = await Test.createTestingModule({
-      providers: [
-        CreateUserHandler,
-        {
-          provide: EventBus,
-          useValue: {
-            publish: jest.fn(),
-          },
-        },
-        {
-          provide: getRepositoryToken(UserEntity),
-          useValue: {
-            findOne: jest.fn().mockResolvedValue(null),
-          },
-        },
-        {
-          provide: getConnectionToken(),
-          useValue: { transaction: jest.fn() },
-        },
-        {
-          provide: UserFactory,
-          useValue: {
-            create: jest.fn(),
-          },
-        },
-        {
-          provide: UserRepository,
-          useValue: {
-            save: jest.fn().mockResolvedValue({}),
-          },
-        },
-      ],
-    }).compile();
+//   beforeAll(async () => {
+//     const module = await Test.createTestingModule({
+//       providers: [
+//         CreateUserHandler,
+//         {
+//           provide: EventBus,
+//           useValue: {
+//             publish: jest.fn(),
+//           },
+//         },
+//         {
+//           provide: getRepositoryToken(UserEntity),
+//           useValue: {
+//             findOne: jest.fn().mockResolvedValue(null),
+//           },
+//         },
+//         {
+//           provide: getConnectionToken(),
+//           useValue: { transaction: jest.fn() },
+//         },
+//         {
+//           provide: 'UserRepository',
+//           useValue: {
+//             save: jest.fn(),
+//           },
+//         },
+//         {
+//           provide: UserFactory,
+//           useValue: {
+//             create: jest.fn(),
+//           },
+//         },
+//       ],
+//     }).compile();
 
-    createUserHandler = module.get(CreateUserHandler);
-    userRepository = module.get(UserRepository);
-    userFactory = module.get(UserFactory);
-  });
+//     createUserHandler = module.get(CreateUserHandler);
+//     userRepository = module.get('UserRepository');
+//     userFactory = module.get(UserFactory);
+//   });
 
-  const name = '22tome';
-  const email = 'to22me@gmail.com';
-  const password = 'pass1234';
-  const signupVerifyToken = uuid.v1();
+//   const name = '22tome';
+//   const email = 'to22me@gmail.com';
+//   const password = 'pass1234';
+//   const signupVerifyToken = uuid.v1();
 
-  describe('execute', () => {
-    it('should execute CreateUserCommand', async () => {
-      // Given
-      userRepository.findByEmail = jest.fn().mockResolvedValue(null);
-      // When
-      await createUserHandler.execute(
-        new CreateUserCommand(name, email, password),
-      );
-      // Then
-      expect(userRepository.save).toHaveBeenCalled();
-    });
-  });
-});
+//   describe('execute', () => {
+//     it('should execute CreateUserCommand', async () => {
+//       // Given
+//       userRepository.findByEmail = jest.fn().mockResolvedValue(null);
+//       // When
+//       await createUserHandler.execute(
+//         new CreateUserCommand(name, email, password),
+//       );
+//       // Then
+//       expect(userRepository.save).toBeCalledWith();
+//     });
+//   });
+// });
---
+++ src/users/application/command/login.handler.spec.ts
@@ -0,0 +1,69 @@
+import { LoginHandler } from './login.handler';
+import { AuthService } from 'src/auth/auth.service';
+import { Test } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { UserEntity } from 'src/users/infra/db/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { LoginCommand } from './login.command';
+import { NotFoundException } from '@nestjs/common';
+
+describe('LoginHandler', () => {
+  let loginHandler: LoginHandler;
+  let authService: AuthService;
+  let userRepository: Repository<UserEntity>;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        LoginHandler,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    loginHandler = module.get(LoginHandler);
+    authService = module.get(AuthService);
+    userRepository = module.get(getRepositoryToken(UserEntity));
+  });
+
+  const email = 'to22me@gmail.com';
+  const password = 'pass1234';
+  const name = undefined;
+  const id = undefined;
+
+  describe('execute', () => {
+    it('should throw NotFoundException when user does not exist', async () => {
+      // Given
+
+      // When
+      const loginCommand = new LoginCommand(email, password);
+      const executePromise = loginHandler.execute(loginCommand);
+      // Then
+      await expect(executePromise).rejects.toThrowError(NotFoundException);
+    });
+
+    it('should login user when user exists', async () => {
+      // Given
+      userRepository.findOne = jest.fn().mockResolvedValue({
+        email,
+        password,
+      });
+
+      // When
+      const executePromise = loginHandler.execute(loginCommand);
+      await loginHandler.execute(loginCommand);
+      // Then
+      expect(authService.login).toBeCalledWith({ email, id, name });
+    });
+  });
+});
